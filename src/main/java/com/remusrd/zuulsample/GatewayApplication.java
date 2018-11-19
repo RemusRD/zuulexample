2b93662ab715a4838b4869ade5795212ba0084ed
Remus Richard Dumitrache <Remus.Richard@autentia.com>
2018-11-19 16:32:33 +0100

    added eureka config

--- src/main/java/com/remusrd/zuulsample/GatewayApplication.java
+++ src/main/java/com/remusrd/zuulsample/GatewayApplication.java
@@ -3,14 +3,16 @@ package com.remusrd.zuulsample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class GatewayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
 
+	}
 }
